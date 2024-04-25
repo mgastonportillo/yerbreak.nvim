8ae2e86ce4ed6cf8c262089caa65cf164ad4aab1
mgastonportillo <mgastonportillo@gmail.com>
2024-04-25 13:24:49 -0300

    Disable scrolling down in buffer

--- lua/yerbreak/utils.lua
+++ lua/yerbreak/utils.lua
@@ -9,7 +9,7 @@ local get_table = function()
 	end
 end
 
-local buffer_opts = function(frame_width, frame_height)
+local get_opts = function(frame_width, frame_height)
 	local ui_height = vim.api.nvim_list_uis()[1].height
 	local ui_width = vim.api.nvim_list_uis()[1].width
 
@@ -27,56 +27,60 @@ local buffer_opts = function(frame_width, frame_height)
 end
 
 local prev_frame = nil
-local pick_random_frame = function(frames_tbl)
+local get_random_frame = function(frames_tbl)
 	local keys = vim.tbl_keys(frames_tbl)
 	local random_key
 	repeat
 		random_key = keys[math.random(#keys)]
-	until random_key ~= prev_frame
+	until frames_tbl[random_key] ~= prev_frame
 	return frames_tbl[random_key]
 end
 
-local get_size = function(content)
-	local width = #content[1] / 3
-	local height = math.min(32, #content)
+local get_size = function(frame)
+	local width = #frame[1] / 3
+	local height = math.min(32, #frame)
 	return width, height
 end
 
 local win_id
 M.open_float = function()
 	local ascii_tbl = get_table()
-	local content = pick_random_frame(ascii_tbl)
-	prev_frame = content
-	local win_opts = buffer_opts(get_size(content))
+	local frame = get_random_frame(ascii_tbl)
+	local win_opts = get_opts(get_size(frame))
 	local new_buffer = vim.api.nvim_create_buf(false, true)
 	win_id = vim.api.nvim_open_win(new_buffer, true, win_opts)
 	local bufnr = vim.fn.bufnr()
 
-	vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, content)
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, frame)
 	vim.api.nvim_buf_set_name(bufnr, "yerbreak")
 	-- Effectively erase the buffer from memory
 	vim.api.nvim_buf_set_option(bufnr, "bufhidden", "delete")
+	-- Prevent scrolling
+	vim.api.nvim_win_set_option(win_id, "scrolloff", 999)
+	vim.api.nvim_win_set_option(win_id, "wrap", false)
 
-	local update_content
-	update_content = function()
+	local update_frame
+	update_frame = function()
 		for _, win in ipairs(vim.api.nvim_list_wins()) do
 			-- Execute code only if win_id is open
 			if win == win_id then
-				content = pick_random_frame(ascii_tbl)
-				local new_win_opts = buffer_opts(get_size(content))
+				-- Store prev frame
+				prev_frame = frame
+				frame = get_random_frame(ascii_tbl)
+				local new_win_opts = get_opts(get_size(frame))
 				vim.api.nvim_win_set_config(win_id, new_win_opts)
-				vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, content)
+				vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, frame)
 				-- Recursively pick a new frame
-				vim.defer_fn(update_content, 3000)
+				vim.defer_fn(update_frame, 3000)
 			end
 		end
 	end
 
-	update_content()
+	update_frame()
 
 	-- Display exit message if force closed
-	vim.api.nvim_create_autocmd("WinClosed", {
-		pattern = "<buffer=" .. new_buffer .. ">",
+	vim.api.nvim_create_autocmd("BufUnload", {
+		pattern = "yerbreak",
 		callback = function()
 			print("Yerbreak is off :(")
 		end,
